899276b28cb7359ee1ea30f7b8f61bbf64263faa
Guillaume <guillaume@oss.ventures>
2022-11-18 16:01:23 +0100

    Setup Prettier

--- pages/client-register.vue
+++ pages/client-register.vue
@@ -1,7 +1,7 @@
 <script  lang='ts' setup>
 // const data = await useFetch('/api/client')
-async function registerClient(input:IInputClient)  { 
-    await $fetch( '/api/client/register', {
+async function registerClient(input: IInputClient) {
+    await $fetch('/api/client/register', {
         method: 'POST',
         body: data
     });
@@ -17,41 +17,24 @@ const data = reactive({
 <template>
     <div>
         <h1>Clients</h1>
-      <form
-        id="app"
-        method="post"
-        @submit="registerClient"
-        >
-        <p>
-            <label for="age">Name</label>
-            <input
-            id="name"
-            v-model="data.name"
-            type="text"
-            >
-        </p>
-        <p>
-            <label for="age">email</label>
-            <input
-            id="age"
-            v-model="data.email"
-            type="test"
-          
-            >
-        </p>
-        <p>
-            <label for="age">password</label>
-            <input
-            id="age"
-            v-model="data.password"
-            type="password"
-            >
-        </p>
-        <p>
-            <input type="submit" value="Submit">
-        </p>
-        
-      </form>
+        <form id="app" method="post" @submit="registerClient">
+            <p>
+                <label for="age">Name</label>
+                <input id="name" v-model="data.name" type="text">
+            </p>
+            <p>
+                <label for="age">email</label>
+                <input id="age" v-model="data.email" type="test">
+            </p>
+            <p>
+                <label for="age">password</label>
+                <input id="age" v-model="data.password" type="password">
+            </p>
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+
+        </form>
 
     </div>
 </template>
